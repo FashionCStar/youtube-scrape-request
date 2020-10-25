f65693739845a8a22d9ebaeac389d4644b7bd818
FashionCStar <fashioncstar@gmail.com>
2020-10-25 10:48:13 -0700

    youtube paginatin fix

--- scraper.js
+++ scraper.js
@@ -109,14 +109,6 @@ function parseOldFormat($, vid) {
         "channel_link": `https://www.youtube.com${$byline.find("a").attr("href")}`,
     };
 
-    // let uploader = {
-    //     "username": $byline.text(),
-    //     "url": `https://www.youtube.com${$byline.find("a").attr("href")}`,
-    //     "verified": !!$byline.find("[title=Verified]").length
-    // };
-
-    // // Return json
-    // return { video: video, uploader: uploader };
     return video
 }
 
@@ -131,18 +123,9 @@ function parseJsonFormat(contents, json) {
             if (sectionList.hasOwnProperty("itemSectionRenderer")) {
                 sectionList.itemSectionRenderer.contents.forEach(content => {
                     try {
-                        // if (content.hasOwnProperty("channelRenderer")) {
-                        //     json.results.push(parseChannelRenderer(content.channelRenderer));
-                        // }
                         if (content.hasOwnProperty("videoRenderer")) {
                             json.results.push(parseVideoRenderer(content.videoRenderer));
                         }
-                        // if (content.hasOwnProperty("radioRenderer")) {
-                        //     json.results.push(parseRadioRenderer(content.radioRenderer));
-                        // }
-                        // if (content.hasOwnProperty("playlistRenderer")) {
-                        //     json.results.push(parsePlaylistRenderer(content.playlistRenderer));
-                        // }
                     }
                     catch(ex) {
                         console.error("Failed to parse renderer:", ex);
@@ -159,72 +142,6 @@ function parseJsonFormat(contents, json) {
             console.log(sectionList);
         }
     });
-}
-
-/**
- * Parse a channelRenderer object from youtube search results
- * @param {object} renderer - The channel renderer
- * @returns object with data to return for this channel
- */
-function parseChannelRenderer(renderer) {
-    let channel = {
-        "id": renderer.channelId,
-        "title": renderer.title.simpleText,
-        "url": `https://www.youtube.com${renderer.navigationEndpoint.commandMetadata.webCommandMetadata.url}`,
-        "snippet": renderer.descriptionSnippet ? renderer.descriptionSnippet.runs.reduce(comb, "") : "",
-        "thumbnail_src": renderer.thumbnail.thumbnails[renderer.thumbnail.thumbnails.length - 1].url,
-        "video_count": renderer.videoCountText ? renderer.videoCountText.runs.reduce(comb, "") : "",
-        "subscriber_count": renderer.subscriberCountText ? renderer.subscriberCountText.simpleText : "0 subscribers",
-        "verified": renderer.ownerBadges &&
-                    renderer.ownerBadges.some(badge => badge.metadataBadgeRenderer.style.indexOf("VERIFIED") > -1) || 
-                    false
-    };
-
-    return { channel };
-}
-
-/**
- * Parse a playlistRenderer object from youtube search results
- * @param {object} renderer - The playlist renderer
- * @returns object with data to return for this playlist
- */
-function parsePlaylistRenderer(renderer) {
-    let thumbnails = renderer.thumbnailRenderer.playlistVideoThumbnailRenderer.thumbnail.thumbnails;
-    let playlist = {
-        "id": renderer.playlistId,
-        "title": renderer.title.simpleText,
-        "url": `https://www.youtube.com${renderer.navigationEndpoint.commandMetadata.webCommandMetadata.url}`,
-        "thumbnail_src": thumbnails[thumbnails.length - 1].url,
-        "video_count": renderer.videoCount
-    };
-
-    let uploader = {
-        "username": renderer.shortBylineText.runs[0].text,
-        "url": `https://www.youtube.com${renderer.shortBylineText.runs[0].navigationEndpoint.commandMetadata.webCommandMetadata.url}`
-    };
-
-    return { playlist: playlist, uploader: uploader };
-}
-
-/**
- * Parse a radioRenderer object from youtube search results
- * @param {object} renderer - The radio renderer
- * @returns object with data to return for this mix
- */
-function parseRadioRenderer(renderer) {
-    let radio = {
-        "id": renderer.playlistId,
-        "title": renderer.title.simpleText,
-        "url": `https://www.youtube.com${renderer.navigationEndpoint.commandMetadata.webCommandMetadata.url}`,
-        "thumbnail_src": renderer.thumbnail.thumbnails[renderer.thumbnail.thumbnails.length - 1].url,
-        "video_count": renderer.videoCountText.runs.reduce(comb, "")
-    };
-
-    let uploader = {
-        "username": renderer.shortBylineText ? renderer.shortBylineText.simpleText : "YouTube"
-    };
-
-    return { radio: radio, uploader: uploader };
 }
 
 /**
@@ -250,15 +167,6 @@ function parseVideoRenderer(renderer) {
             (renderer.publishedTimeText ? "0 views" : "0 watching")
     };
 
-    // let uploader = {
-    //     "username": renderer.ownerText.runs[0].text,
-    //     "url": `https://www.youtube.com${renderer.ownerText.runs[0].navigationEndpoint.commandMetadata.webCommandMetadata.url}`
-    // };
-    // uploader.verified = renderer.ownerBadges &&
-    //     renderer.ownerBadges.some(badge => badge.metadataBadgeRenderer.style.indexOf("VERIFIED") > -1) || 
-    //     false;
-
-    // return { video: video, uploader: uploader };
     return video
 }
 
